39b9c62eedac14558fa0f29ce509d7759e4922d7
Juan David Peña Lagos <94989889+juandpl18@users.noreply.github.com>
2026-09-18 00:12:13 -0500

    Add files via upload

---
+++ Base de datos/smartgrid.sql
@@ -0,0 +1,31 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict QvLnNI6QGmWK7CvGTSVNcqdJzodnL2j3N45muGQtE6YkF9R5kd9SD1c8nBnvfvb
+
+-- Dumped from database version 18.6
+-- Dumped by pg_dump version 18.6
+
+-- Started on 2026-09-18 00:10:47
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+-- Completed on 2026-09-18 00:10:48
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict QvLnNI6QGmWK7CvGTSVNcqdJzodnL2j3N45muGQtE6YkF9R5kd9SD1c8nBnvfvb
+
